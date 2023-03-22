824bdb16e8c6af9b3fc8d7f9c1d6d7c5f25ca557
Max Carvalho <120032100+maxrscarvalho@users.noreply.github.com>
2023-03-22 17:12:10 -0300

    inicial

--- AULAS/Aula_133_Encapsulamento_(modificadores_de_acesso_public, _protected, __private).py
+++ AULAS/Aula_133_Encapsulamento_(modificadores_de_acesso_public, _protected, __private).py
@@ -14,11 +14,25 @@ class Foo:
     def __init__(self):             # O self referece a classe de instancia
         self.public = 'isso e publico'
         self._protected = 'isso e protegido'
+        self.__exemplo = 'isso e private'
 
     def metodo_publico(self):
+        # self._metodo_protected()
+        # print(self._protected)
+        print(self._protected)
         return 'metodo_publico'
 
+    def _metodo_protected(self):
+        print('_metodo_protected')
+        return '_metodo_protected'
+
+    def _metodo_private(self):
+        print('_metodo_private')
+        return '_metodo_private'
+
+
 f = Foo()
-print(f._protected)
+# print(f._protected)
 # print(f.public())
-# print(f.metodo_publico())
+print(f.metodo_publico())
+print(f._Foo__metodo_private)
